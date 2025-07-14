64e158f92a79a8e48ffbd11fc8ed68553126e827
Kai Miyamoto <kayoosh2009@gmail.com>
2025-07-14 18:56:44 +0300

    Delete src directory

--- src/components/Timeline.tsx
+++
@@ -1,7 +0,0 @@
-export function Timeline() {
-  return (
-    <div className="h-full bg-zinc-800 p-2 text-xs text-white">
-      <p className="text-center">🎞️ Timeline (в разработке)</p>
-    </div>
-  );
-}
